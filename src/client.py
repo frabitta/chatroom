d232b1f833950abd6228d740e3b9f8d95ad6e09f
Francesco Bittasi <francesco.bittasi@studio.unibo.it>
2024-05-14 19:00:16 +0200

    client sends name

--- src/client.py
+++ src/client.py
@@ -9,18 +9,24 @@ BUFFSIZE = 1024
 QUIT_COMMAND = "?{quit}"
 ADDR = (HOST, PORT)
 
-def receiver(my_socket):
+client_name = "USR1_LISTENER"
+
+def receiver(client_socket):
     while True:
         try:
-            msg = my_socket.recv(BUFFSIZE).decode("utf8")
+            msg = client_socket.recv(BUFFSIZE).decode("utf8")
             print(msg)
         except OSError:
             break
 
 def connect(addr):
-    my_socket = socket(AF_INET, SOCK_STREAM)
-    my_socket.connect(ADDR)
-    Thread(target=receiver, args=(my_socket,)).start()
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect(ADDR)
+    client_socket.send(bytes(client_name, "utf8"))
+    Thread(target=receiver, args=(client_socket,)).start()
+
+def send_message(client_socket, msg):
+    client_socket.send(bytes(client_name + ": " + msg, "utf8"))
 
 if __name__ == "__main__":
     connect(ADDR)
